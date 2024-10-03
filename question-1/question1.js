55217599f7543385442d57304762336753336500
Fahad <fahedihssan@gmail.com>
2024-10-03 14:34:42 -0400

    Initial commit - Lab Test 1

---
+++ question-1/question1.js
@@ -0,0 +1,15 @@
+async function lowerCaseWords(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('Input must be an array');
+    } else {
+        const filteredWords = arr.filter(item => typeof item === 'string')
+                                 .map(word => word.toLowerCase());
+        return filteredWords;
+    }
+}
+
+const mixedArray = ["PIZZA", 10, true, 25, false, "Wings"];
+
+lowerCaseWords(mixedArray)
+    .then(result => console.log(result))   // Output: ['pizza', 'wings']
+    .catch(error => console.error(error));
